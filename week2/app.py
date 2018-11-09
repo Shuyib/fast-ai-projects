433e5d58c3f5ab5d145828e84e532bbf3fdb5583
Shuyib <benmainye@gmail.com>
2018-11-09 13:58:36 -0800

    add file remnants generated.

--- week2/app.py
+++ week2/app.py
@@ -5,19 +5,21 @@ from fastai import *
 from fastai.vision import *
 from flask import Flask, jsonify, request, render_template
 from werkzeug.exceptions import BadRequest
-from hints import fact_finder
+
+
+fastai.defaults.device = torch.device('cpu')
 
 def evaluate_image(img) -> str:
-    pred_class, pred_idx, outputs = trained_model.predict(img)
-    return pred_class
+	pred_class, pred_idx, outputs = trained_model.predict(img)
+	return pred_class
 
 def load_model():
-    path = '/floyd/home'
-    classes = ['eosinophil', 'lymphocyte', 'monocyte', 'neutrophil']
-    data = ImageDataBunch.single_from_classes(path, classes, tfms=get_transforms(), size=220).normalize(imagenet_stats)
-    learn = create_cnn(data, models.resnet50)
-    learn.load('stage-5')
-    return learn
+	path = '/floyd/home'
+	classes = ['eosinophil', 'lymphocyte', 'monocyte', 'neutrophil']
+	data = ImageDataBunch.single_from_classes(path, classes, tfms=get_transforms(), size=220).normalize(imagenet_stats)
+	learn = create_cnn(data, models.resnet50)
+	learn.load('/floyd/home/models/stage-5')
+	return learn
 
 app = Flask(__name__)
 app.config['DEBUG'] = False
@@ -26,7 +28,7 @@ trained_model = load_model()
 @app.route('/', methods=['GET'])
 def index():
     """Render the app"""
-    return render_template('serving_template.html')
+    return render_template('/floyd/home/templates/serving_template.html')
 
 @app.route('/image', methods=['POST'])
 def eval_image():
@@ -50,4 +52,4 @@ def eval_image():
     })
 
 if __name__ == "__main__":
-app.run(host='0.0.0.0', threaded=False)
+	app.run(host='0.0.0.0', threaded=False)
